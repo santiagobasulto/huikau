6ea73e0ff5a8d15c780225f152c7b29c5aee7981
Santiago Basulto <santiago.basulto@gmail.com>
2012-01-11 15:51:02 -0300

    .gitignore added

---
+++ AdServer.py
@@ -0,0 +1,3 @@
+from lib import webapp2
+class AdServer(webapp2.RequestHandler):
+    pass
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from lib import webapp2
-
+import AdServer.AdServer
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, webapp2!')
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
+    ('/adserver', AdServer.AdServer),
 ], debug=True)
 
 def main():
